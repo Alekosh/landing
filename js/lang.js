d19d740f4e6a2d146cc47e3cb05af6502e8ece71
Alekosh <aakhosh00@gmail.com>
2023-09-03 18:26:40 +0500

    complete

---
+++ js/lang.js
@@ -0,0 +1,32 @@
+const langArr = {
+  "Home": {
+    "ru": "Главная",
+    "en": "Home",
+  },
+  "About": {
+    "ru": "О нас",
+    "en": "About us",
+  },
+  "Skills": {
+    "ru": "Скилл",
+    "en": "Skills",
+  },
+  "Portfolio": {
+    "ru": "Портфолио",
+    "en": "Portfolio",
+  },
+  "Contacts": {
+    "ru": "Контакты",
+    "en": "Contacts",
+  },
+  //=========
+  "Name": {
+    "ru": "Денис Новик",
+    "en": "Denis Novik",
+  },
+  "NameAbout": {
+    "ru": "UX | UI Дизайнер<br>24 лет, Минск",
+    "en": "UX | UI designer<br>24 years old, Minsk",
+  },
+  //=========
+}
